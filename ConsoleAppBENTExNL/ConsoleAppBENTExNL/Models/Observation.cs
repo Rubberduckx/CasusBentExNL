c1022094307942e9092077a28650fe37ef82671a
xavier1803 <56248472+xavier1803@users.noreply.github.com>
2024-10-31 11:24:22 +0100

    Adds, observation, species and area classes/methodes

--- ConsoleAppBENTExNL/ConsoleAppBENTExNL/Models/Observation.cs
+++ ConsoleAppBENTExNL/ConsoleAppBENTExNL/Models/Observation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppBENTExNL.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,36 +7,60 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppBENTExNL.Models
 {
-    internal class Observation
-    {
-        private int id;
-        private double lat;
-        private double lng;
-        private string image;
-        private string description;
-        private Species species;
-        private User user;
-        private Area area;
+	internal class Observation
+	{
+		private int id;
+		private double lat;
+		private double lng;
+		private string image;
+		private string description;
+		private Species species;
+		private User user;
+		private Area area;
 
-        public Observation(int id, double lat, double lng, Species species, User user, Area area = null, string image = "", string description = "")
-        {
-            this.id = id;
-            this.lat = lat;
-            this.lng = lng;
-            this.species = species;
-            this.user = user;
-            this.area = area;
-            this.image = image;
-            this.description = description;
-        }
+		public Observation(int id, double lat, double lng, Species species, User user, Area area, string image = "", string description = "")
+		{
+			this.id = id;
+			this.lat = lat;
+			this.lng = lng;
+			this.species = species;
+			this.user = user;
+			this.area = area;
+			this.image = image;
+			this.description = description;
+		}
 
-        public int GetId() => id;
-        public double GetLat() => lat;
-        public double GetLong() => lng;
-        public string GetImage() => image;
-        public string GetDescription() => description;
-        public Species GetSpecies() => species;
-        public User GetUser() => user;
-        public Area GetArea() => area;
-    }
+		public int GetId() => id;
+		public double GetLat() => lat;
+		public double GetLong() => lng;
+		public string GetImage() => image;
+		public string GetDescription() => description;
+		public Species GetSpecies() => species;
+		public User GetUser() => user;
+		public Area GetArea() => area;
+
+		public void CreateObservation(Observation observation)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.CreateObservation(observation);
+		}
+
+		public void GetObservation(int id)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.GetObservation(id);
+		}
+
+		public void UpdateObservation(Observation observation)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.UpdateObservation(observation);
+		}
+
+		public void DeleteObservation(int id)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.DeleteObservation(id);
+		}
+	}
 }
--- ConsoleAppBENTExNL/ConsoleAppBENTExNL/Models/Species.cs
+++ ConsoleAppBENTExNL/ConsoleAppBENTExNL/Models/Species.cs
@@ -1,32 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleAppBENTExNL.DAL;
+using System;
 
 namespace ConsoleAppBENTExNL.Models
 {
-    internal class Species
-    {
-        private int id;
-        private string name;
-        private string type;
-        private string description;
-        private int size;
+	internal class Species
+	{
+		private int id;
+		private string name;
+		private string type;
+		private string description;
+		private int size;
 
-        public Species(int id, string name, string type, string description, int size)
-        {
-            this.id = id;
-            this.name = name;
-            this.type = type;
-            this.description = description;
-            this.size = size;
-        }
+		public Species(int id, string name, string type, string description, int size)
+		{
+			this.id = id;
+			this.name = name;
+			this.type = type;
+			this.description = description;
+			this.size = size;
+		}
 
-        public int GetId() => id;
-        public string GetName() => name;
-        public string GetType() => type;
-        public string Description() => description;
-        public int GetSize() => size;
-    }
+		public int GetId() => id;
+		public string GetName() => name;
+		public string GetType() => type;
+		public string GetDescription() => description;
+		public int GetSize() => size;
+
+		public void CreateSpecies(Species species)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.CreateSpecies(species);
+		}
+
+		public void GetSpecies(int id)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.GetSpecies(id);
+		}
+
+		public void UpdateSpecies(Species species)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.UpdateSpecies(species);
+		}
+
+		public void DeleteSpecies(int id)
+		{
+			SQLDAL sqldal = SQLDAL.GetSingleton();
+			sqldal.DeleteSpecies(id);
+		}
+	}
 }
